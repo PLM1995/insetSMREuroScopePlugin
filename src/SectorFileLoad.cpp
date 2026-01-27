e07ca35829d893589e6791574e934057b50be195
Peter Mooney <61326713+PLM1995@users.noreply.github.com>
2026-01-27 15:11:28 +0000

    Add copyright notice and update readme.

--- src/SectorFileLoad.cpp
+++ src/SectorFileLoad.cpp
@@ -1,4 +1,11 @@
-#include "SectorFileLoad.h"
+/*
+ * Copyright (C) 2026 @PLM1995
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ */
+
+ #include "SectorFileLoad.h"
 #include "InsetSMR.h"
 #include <fstream>
 #include <filesystem>
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2026 @PLM1995
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ */
+
 #include <memory>
 #pragma warning(push, 0)
 #include <EuroScopePlugIn.h>
